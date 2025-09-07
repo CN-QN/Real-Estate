35c351d4dce636b97dc4e4428eda06cd0faa7f7a
tthuy05 <129072778+tthuy05@users.noreply.github.com>
2025-09-07 23:13:51 +0700

    Completed authentication functionality and added login/register views

---
+++ RealEstate/Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using RealEstate.Models;  
+namespace RealEstate.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext(); 
+
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+              
+                var user = _context.Users.FirstOrDefault(u => u.Username == model.Username);
+
+                if (user != null && user.Password == model.Password) 
+                {
+                    
+                    Session["User"] = user;
+                    return RedirectToAction("Index", "Home"); 
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                }
+            }
+            return View(model);
+        }
+
+        
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var existingUser = _context.Users.FirstOrDefault(u => u.Username == model.Username);
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError("", "Username already exists.");
+                    return View(model);
+                }
+
+                
+                var newUser = new User
+                {
+                    Username = model.Username,
+                    Password = model.Password 
+                };
+
+              
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+
+                return RedirectToAction("Login"); 
+            }
+
+            return View(model);
+        }
+
+        // GET: Logout
+        public ActionResult Logout()
+        {
+            Session.Clear(); 
+            return RedirectToAction("Index", "Home"); 
+        }
+    }
+}
---
+++ RealEstate/models/RegisterModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace RealEstate.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, ErrorMessage = "Password must be at least 6 characters long.", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
