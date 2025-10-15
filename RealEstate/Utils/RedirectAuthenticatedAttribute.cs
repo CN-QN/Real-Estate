223cad19819312587d6083e7759c263a27d1a29f
HoQuocNam92 <hoquocnam92@gmail.com>
2025-10-15 20:46:56 +0700

    fix : correct authorize login

---
+++ RealEstate/Utils/RedirectAuthenticatedAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.Utils
+{
+    public class RedirectAuthenticatedAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                       new System.Web.Routing.RouteValueDictionary(
+                    new { controller = "Property", action = "Index" }
+                ));
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
